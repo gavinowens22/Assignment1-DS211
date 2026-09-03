8027fe54a9cf030c6bd6c2d31a654d51d7f92c9a
gavinowens22 <107884409+gavinowens22@users.noreply.github.com>
2026-09-02 23:35:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import csv
+from car import Car
+from seller import Seller
 
+sellers = {}
+
+with open("Cars_Data.csv", "r") as file:
+    reader = csv.DictReader(file)
+
+    for row in reader:
+        car = Car(
+            row["manufacturer"],
+            row["model"],
+            row["year"],
+            row["mileage"],
+            row["engine"],
+            row["transmission"],
+            row["drivetrain"],
+            row["mpg"],
+            row["exterior_color"],
+            row["interior_color"],
+            row["accidents_or_damage"],
+            row["price"]
+        )
+
+        seller_name = row["seller_name"]
+        seller_rating = row["seller_rating"]
+
+        if seller_name not in sellers:
+            sellers[seller_name] = Seller(seller_name, seller_rating)
+
+        sellers[seller_name].buy(car)
