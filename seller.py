850890cddeb784575e512ce3ec67f1275656b0b2
gavinowens22 <107884409+gavinowens22@users.noreply.github.com>
2026-09-02 23:25:30 -0400

    Update seller.py

--- seller.py
+++ seller.py
@@ -1,1 +1,14 @@
+class Seller:
+  def __init__(self,name,rating):
+    self.name = name
+    self.rating = rating
+    self.inventory = []
 
+  def buy(self,car):
+      if car not in self.inventory:
+            self.inventory.append(car)
+
+  def sell(self,car):
+      if car in self.inventory:
+        self.inventory.remove(car)
+ 
